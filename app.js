295e3770cf9668abae3daa383992268793e88a63
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-10-13 02:05:09 +0200

    feat: updated server.js

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-const express = require('express');
-const cors = require('cors');
-const schoolRoutes = require('./src/router/schoolRoutes')
-const connection = require('./src/database/connection')
 
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/mz/schools', schoolRoutes);
-app.use('/', (req, res) => {
-    res.json({ msg: "Hello World" })
-});
-
-module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,21 @@
-const app = require("./app.js")
 
+const express = require('express');
+const cors = require('cors');
+const schoolRoutes = require('./src/router/schoolRoutes')
+const connection = require('./src/database/connection')
 require('dotenv').config();
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/mz/schools', schoolRoutes);
+app.use('/', (req, res) => {
+    res.json({ msg: "Hello World" })
+});
+
 const port = process.env.PORT || "5001"
 
 app.listen(port, () => {
